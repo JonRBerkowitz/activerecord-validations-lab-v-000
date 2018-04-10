9cdb3ff6a401e963fd08371245b21ae88bc740c7
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-04-10 14:04:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,19 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 100 }
   validates :summary, length: {maximum: 25 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity
+
+  CLICKBAIT = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def clickbaity
+    if CLICKBAIT.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
